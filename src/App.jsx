5b629da2ad634108d6b1febe47e844e2259cb933
krishnathakkar29 <work.krishnat@gmail.com>
2024-05-18 00:42:55 +0530

    Sidebar and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import SideNav from "./components/SideNav";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="w-64 fixed">
+        <SideNav selectedIndex={(valIdx) => console.log(valIdx)} />
+      </div>
+      <div className="ml-64">Body</div>
     </>
   );
 }
